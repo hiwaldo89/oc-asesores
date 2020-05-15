45427a9508a59a0e52f552b70c1fdcd7a5dc0cba
Oswaldo Zarate <2448407-hiwaldo89@users.noreply.gitlab.com>
2020-05-15 13:27:45 -0500

    change third slide and add offline mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `OC Asesores`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Somos un despacho fiscal contable, te asesoramos y trabajamos para que mantengas tus finanzas sanas.`,
+    author: `Bloom Studio`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -32,6 +32,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
